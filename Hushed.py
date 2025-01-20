abce3d16e196735e4c03824a6fc3e39c1adb166e
MoofireX <suraj.saripalli13@gmail.com>
2025-01-19 18:20:34 -0600

    BeautifulSoup used

--- Hushed.py
+++ Hushed.py
@@ -1,40 +1,14 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.options import Options
-import time
+from bs4 import BeautifulSoup
+import requests
+import urllib
+from pydub import AudioSegment as audio
+from pydub.playback import play
 
-chrome_options = Options()
-chrome_options.add_argument("--headless")  # Run in headless mode (no GUI)
-chrome_options.add_argument("--disable-gpu")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36")
+url = input("What type of bg music would you like? The choices are Lofi, Electrical, and Instrumental. ")
 
-service = Service('~/moofire/chromedriver-linux64/chromedriver')  # Specify path to chromedriver
-driver = webdriver.Chrome(service=service, options=chrome_options)
+link = f"https://www.chosic.com/free-music/{url.lower()}"
+link = urllib.request.urlopen(url)
 
-music = input("What type of music would you like to play? The options are Instrumental, LoFi, and Electric. ")
-url = ""
+content = link.read()
+soup = BeautifulSoup(content, "html.parser")
 
-if music == "Instrumental":
-    url = "https://www.youtube.com/results?search_query=instrumental"
-elif music == "LoFi":
-    url = "https://www.youtube.com/results?search_query=LoFi"
-elif music == "Electric":
-    url = "https://www.youtube.com/results?search_query=electronic+background+music"
-
-driver.get(url)
-time.sleep(5)
-
-vid_links = []
-vids = driver.find_elements(By.CSS_SELECTOR, "a#video-title")
-for vid in vids[:100]:
-    link = vid.get_attribute("href")
-
-    if link:
-        vid_links.append(link)
-
-driver.quit()
-
-for indx, link in enumerate(vid_links):
-    print(f"Video {indx + 1}: link")
