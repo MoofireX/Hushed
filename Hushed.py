80c0c5858c5504ced8a0dceb45a8c7df7eb7f484
MoofireX <suraj.saripalli13@gmail.com>
2025-01-19 18:47:46 -0600

    More

--- Hushed.py
+++ Hushed.py
@@ -4,11 +4,16 @@ import urllib
 from pydub import AudioSegment as audio
 from pydub.playback import play
 
+headers = {
+	"User-Agent" : "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/131.0.0.0 Safari/537.36"}
+
 url = input("What type of bg music would you like? The choices are Lofi, Electrical, and Instrumental. ")
 
 link = f"https://www.chosic.com/free-music/{url.lower()}"
-link = urllib.request.urlopen(url)
+link = urllib.request.urlopen(link)
 
 content = link.read()
 soup = BeautifulSoup(content, "html.parser")
 
+for mp3 in soup.find_all('audio'):
+	print(mp3)
